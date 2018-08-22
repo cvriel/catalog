64c87094f7d81d378d34c0fc8895b066bc6908ba
Stephan Preeker <stephan@preeker.net>
2018-08-22 17:28:05 +0200

    localy working webconnector with auth

--- src/webconnectors/js/signal.js
+++ src/webconnectors/js/signal.js
@@ -1,4 +1,7 @@
+import { getAuthHeaders } from "./auth.js";
+
 (function() {
+
   // Create the connector object
   var myConnector = tableau.makeConnector();
 
@@ -102,13 +105,50 @@
     ]);
   };
 
+  myConnector.init = function(initCallback) {
+    tableau.authType = tableau.authTypeEnum.custom;
+
+    // If we are in the auth phase we only want to show the UI needed for auth
+    if (tableau.phase == tableau.phaseEnum.authPhase) {
+      $("#getvenuesbutton").css("display", "none");
+    }
+
+    if (tableau.phase == tableau.phaseEnum.gatherDataPhase) {
+      // If API that WDC is using has an enpoint that checks
+      // the validity of an access token, that could be used here.
+      // Then the WDC can call tableau.abortForAuth if that access token
+      // is invalid.
+    }
+
+    var accessToken = getAuthHeaders().Authorization;
+    var hasAuth = (accessToken && accessToken.length > 0) || tableau.password.length > 0;
+    updateUIWithAuthState(hasAuth);
+
+    initCallback();
+
+    // If we are not in the data gathering phase, we want to store the token
+    // This allows us to access the token in the data gathering phase
+    if (tableau.phase == tableau.phaseEnum.interactivePhase || tableau.phase == tableau.phaseEnum.authPhase) {
+      if (hasAuth) {
+        tableau.password = accessToken;
+
+        if (tableau.phase == tableau.phaseEnum.authPhase) {
+          // Auto-submit here if we are in the auth phase
+          tableau.submit();
+        }
+
+        return;
+      }
+    }
+  };
+
   // Download the data
   myConnector.getData = function(table, doneCallback) {
   // var dateObj = JSON.parse(tableau.connectionData),
   // dateString = "starttime=" + dateObj.startDate + "&endtime=" + dateObj.endDate,
 
-    //var apiCall = "https://api.data.amsterdam.nl/signals/signal/";  // + dateString + "";
-    var apiCall = "http://localhost:8889/localhost:8000/signals/auth/signal/";  // + dateString + "";
+    // var apiCall = "https://api.data.amsterdam.nl/signals/signal/";  // + dateString + "";
+    var apiCall = "http://localhost:8760/localhost:8000/signals/auth/signal/";  // + dateString + "";
 
     var params = {
       "format": "json",
@@ -125,6 +165,12 @@
     function getPage(page) {
 
       params.page = page;
+
+      //set auth headers
+
+      $.ajaxSetup({
+        headers : { "Authorization": tableau.password }
+      });
 
       promises.push($.getJSON(apiCall, params, function(resp) {
 
@@ -207,13 +253,9 @@
 
   };
 
-  tableau.registerConnector(myConnector);
-
   // This function togglels the label shown depending
   // on whether or not the user has been authenticated
   function updateUIWithAuthState(hasAuth) {
-    console.log("test");
-    console.log(hasAuth);
     if (hasAuth) {
       $(".notsignedin").css("display", "none");
       $(".signedin").css("display", "block");
@@ -221,6 +263,7 @@
       $(".notsignedin").css("display", "block");
       $(".signedin").css("display", "none");
     }
+
   }
 
   $(document).ready(function() {
@@ -229,10 +272,19 @@
       tableau.submit(); // This sends the connector object to Tableau
     });
 
-    updateUIWithAuthState(false);
+    window.auth.initAuth();
+
+    var hasauth = window.auth.isAuthenticated();
+    updateUIWithAuthState(hasauth);
+
     $("#loginButton").click(function() {
+      window.auth.login();
       // console.log("lets login!");
+      //hasauth = window.auth.isAuthenticated();
+      // updateUIWithAuthState(hasauth);
     });
   });
+
+  tableau.registerConnector(myConnector);
 
 })();
