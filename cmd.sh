dd977621be3fec3b8a8a9f9b983df28d62b75ab2
Chris van Riel <c.van.riel@amsterdam.nl>
2020-05-06 11:27:49 +0200

    Changed the translation logic ACC links WFS and WMS
    CMD.sh was changed so the acc. abreviation was not duplicated or even tripled when starting or refreshing
    the page.
    
    Resolves: https://datapunt.atlassian.net/browse/DS-259

--- cmd.sh
+++ cmd.sh
@@ -7,9 +7,13 @@ set -u
 if [[ ${BASE_URL} =~ "acc" ]];
 then
     echo "Converting map.data.amsterdam.nl to acc.map.data.amsterdam.nl"
-    sed -i -e"s;map.data.amsterdam.nl;acc.map.data.amsterdam.nl;" /usr/share/nginx/html/index.html
+    #sed -i -e"s;map.data.amsterdam.nl;acc.map.data.amsterdam.nl;" /usr/share/nginx/html/index.html
+    #2020.05.06 changed the sed so it copes with already converted URL's to acc and not duplicating the .acc abreviation
+    sed -i -e"s;\(https://\)\(.*map.data.amsterdam.nl\)\(.*\);\1acc.map.data.amsterdam.nl\3;" /usr/share/nginx/html/index.html
     echo "Converting t1.data.amsterdam.nl to acc.t1.map.data.amsterdam.nl"
-    sed -i -e"s;t1.data.amsterdam.nl;acc.t1.data.amsterdam.nl;" /usr/share/nginx/html/index.html
+    #sed -i -e"s;t1.data.amsterdam.nl;acc.t1.data.amsterdam.nl;" /usr/share/nginx/html/index.html
+    #2020.05.06 changed the sed so it copes with already converted URL's to acc and not duplicating the .acc abreviation
+    sed -i -e"s;\(https://\)\(.*t1.data.amsterdam.nl\)\(.*\);\1acc.t1.data.amsterdam.nl\3;" /usr/share/nginx/html/index.html
 fi
 
 echo "Converting %BASE_URL% to ${BASE_URL}"
